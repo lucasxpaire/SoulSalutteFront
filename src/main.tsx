05a6334e73c4b27b7e5f0c4feee49fda96e7e612
lucas <lxpaire@inf.ufsm.br>
2025-09-11 11:21:51 -0300

    Arrumando

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
 
-// Configuração do React 18 com StrictMode para melhor development experience
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
